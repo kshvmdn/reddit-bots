82178af68dde7754cade01e9d5f092c9889ab957
Kashav Madan <kshvmdn@gmail.com>
2016-01-16 00:24:41 -0500

    Check if comment body contains misspelling, reply if so

--- tomorrow_corrector/bot.py
+++ tomorrow_corrector/bot.py
@@ -6,12 +6,17 @@ username, password = USERNAME, PASSWORD
 r = praw.Reddit(user_agent='A "tomorrow"-misspelling corrector by /u/tomorrow_corrector')
 r.login(username, password, disable_warning=True)
 
+misspellings = ['tommorow', 'tommorrow', 'tomorow']
+comment_cache = []
 
 def run_bot():
     '''Check /r/all for mispellings in comments and reply to them.'''
     subreddit = r.get_subreddit('all')
     comments = subreddit.get_comments(limit=25)
     for comment in comments:
+        if any(string in comment.body.lower() for string in misspellings) and not comment.id in comment_cache:
+            comment.reply('I think you meant "tomorrow".')
+            comment_cache.append(comment.id)
 
 while True:
     run_bot()
