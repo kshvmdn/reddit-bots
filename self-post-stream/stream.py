19c2f919c6deb11331e927ad3f794424905fc4f3
Kashav Madan <kshvmdn@gmail.com>
2016-01-15 23:22:36 -0500

    Add value check for -limit

--- self-post-stream/stream.py
+++ self-post-stream/stream.py
@@ -4,7 +4,9 @@ import argparse
 parser = argparse.ArgumentParser(description='Stream self posts from any subreddit.')
 parser.add_argument('-sub', required=False, default='all', help='Subreddit name (default=\'all\')')
 parser.add_argument('-limit', required=False, default=100, help='Post limit (default=100, max=1000)', type=int)
+
 args = parser.parse_args()
+args.limit = 1000 if args.limit > 1000 else args.limit
 
 r = praw.Reddit(user_agent='stream only self posts from a sub by /u/km97')
 posts = [post for post in r.get_subreddit(args.sub).get_hot(limit=args.limit) if post.is_self]
