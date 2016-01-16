e5b90eb20ff2835f5359811430fda952e8f7cc01
Kashav Madan <kshvmdn@gmail.com>
2016-01-15 23:11:38 -0500

    Added argparse with arg for subreddit (d=all) & limit (d=100)

--- self-post-stream/stream.py
+++ self-post-stream/stream.py
@@ -1,13 +1,17 @@
 import praw
+import argparse
+
+parser = argparse.ArgumentParser(description='Stream self posts from any subreddit.')
+parser.add_argument('--sub', required=False, default='all', help='Subreddit name (default: `all`)')
+parser.add_argument('--limit', type=int, required=False, default=100, help='Post limit (default: 100)')
+args = parser.parse_args()
 
 r = praw.Reddit(user_agent='stream only self posts from a sub by /u/km97')
-posts = [post for post in r.get_subreddit('all').get_hot(limit=50) if post.is_self]
-
-# print(dir(posts[0]))
+posts = [post for post in r.get_subreddit(args.sub).get_hot(limit=args.limit) if post.is_self]
 
 for post in posts:
     print("Title:", post.title)
-    print("Score: {}, Comments: {}".format(post.score, post.num_comments))
+    print("Score: {} | Comments: {}".format(post.score, post.num_comments))
     print()
     print(post.selftext.replace('**', '').replace('*', ''))
     print()
